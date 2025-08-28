5757a563856a9ea9bf2f59610fa6a2f8de0ade9c
f3l1p3v4 <fvaldez018@gmail.com>
2025-08-28 16:43:53 -0400

    added song

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,7 +1,37 @@
 import { differenceInSeconds } from 'date-fns'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { CyclesContext } from '../../../../contexts/CyclesContext'
 import { CountdownContainer, Separator } from './styles'
+
+function playBeep(frequency: number, duration: number, volume: number = 0.5) {
+  const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)()
+  const oscillator = audioContext.createOscillator()
+  const gainNode = audioContext.createGain()
+
+  oscillator.connect(gainNode)
+  gainNode.connect(audioContext.destination)
+
+  oscillator.type = 'sine'
+  oscillator.frequency.value = frequency
+  gainNode.gain.value = volume
+
+  oscillator.start()
+  oscillator.stop(audioContext.currentTime + duration)
+}
+
+// ADICIONE ESTA FUNÇÃO AQUI (logo após a playBeep)
+function playPomodoroBeeps(secondsRemaining: number) {
+  if (secondsRemaining === 3) {
+    // Primeiro beep (tum)
+    playBeep(440, 0.2, 0.3)
+  } else if (secondsRemaining === 2) {
+    // Segundo beep (tum)
+    playBeep(440, 0.2, 0.3)
+  } else if (secondsRemaining === 1) {
+    // Beep final mais longo (tummmm)
+    playBeep(330, 0.5, 0.4)
+  }
+}
 
 export function Countdown() {
   const {
@@ -13,28 +43,44 @@ export function Countdown() {
   } = useContext(CyclesContext)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const hasPlayedFinalSound = useRef(false)
 
   useEffect(() => {
     let interval: number
-
+  
     if (activeCycle) {
+      // Resetar a ref quando um novo ciclo começar
+      hasPlayedFinalSound.current = false
+      
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
           new Date(activeCycle.startDate),
         )
-
+  
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
-
           setSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
           setSecondsPassed(secondsDifference)
+          
+          // Tocar sons quando faltarem 3, 2 e 1 segundos
+          const secondsRemaining = totalSeconds - secondsDifference
+          
+          // SUBSTITUA ESTE BLOCO IF:
+          if (secondsRemaining <= 3 && secondsRemaining > 0 && !hasPlayedFinalSound.current) {
+            playPomodoroBeeps(secondsRemaining)
+            
+            // Marcar que o som final foi tocado no último segundo
+            if (secondsRemaining === 1) {
+              hasPlayedFinalSound.current = true
+            }
+          }
         }
       }, 1000)
     }
-
+  
     return () => {
       clearInterval(interval)
     }
